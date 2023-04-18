bdbb7c501329dca808cfab42a7a7e5fc7d4a581e
Andre Pilco <andrepilcoch@gmail.com>
2023-04-17 23:11:31 -0500

    update andre

--- frontend/emergencias/src/app/services/backend.service.ts
+++ frontend/emergencias/src/app/services/backend.service.ts
@@ -9,6 +9,6 @@ export class PruebaService {
   constructor(private http: HttpClient) { }
 
   obtenerUsuarios() {
-    return this.http.get('http://3.93.172.158:5000/personal');
+    return this.http.get('http://3.84.29.129:5000/personal');
   }
 }
