bcc2d453462f9ecd97b7a324d234c846aefba4e2
Felipe <u202110902@upc.edu.pe>
2023-04-22 22:41:07 -0500

    fecha

--- frontend/emergencias/src/app/incidentes/incidentes.component.ts
+++ frontend/emergencias/src/app/incidentes/incidentes.component.ts
@@ -151,6 +151,7 @@ export class IncidentesComponent implements OnInit {
           });
           item.fecha_incidente = dateFormat;
           let fecha = item.hora_incidente;
+          // fecha = '000' + fecha;
           const hora = new Date(fecha);
           const horaFormateada = hora.toLocaleTimeString([], {
             hour: '2-digit',
